a8b7d83f052f7cc8373412d9edd3df4011e00f4a
CeAlCuadrado <cealcuadrado@outlook.com>
2022-02-28 11:20:30 -0300

    Instaladas dependencias cypress y ui-switch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { UiSwitchModule } from 'ngx-ui-switch';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
@@ -10,7 +12,10 @@ import { LayoutComponent } from './layout/layout.component';
     LayoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UiSwitchModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
